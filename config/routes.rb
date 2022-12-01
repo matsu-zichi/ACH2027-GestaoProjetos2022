e3712ae1817ff0003ccfa8443ff024a65d87a139
Rodrigo Birocchi <rodrigomarcelo@usp.br>
2022-12-01 14:29:24 -0300

    finalizado

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   get '/users/:user_id/ambientes/:id/adicionar', to: 'ambientes#adicionar_aluno'
   post '/users/:user_id/ambientes/:id/adicionar', to: 'ambientes#redirect_user_adicionado'
+  post '/users/:user_id/ambientes/:id_ambiente/exam/:id_exam/question/:id_question/resposta0', to: 'question#resposta'
   
   root 'home#index'
 end
